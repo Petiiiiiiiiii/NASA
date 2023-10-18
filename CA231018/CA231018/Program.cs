2405deb1e2d7fa84d1960045de3222ec2064d6df
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-10-18 12:29:16 +0200

    Add files via upload

---
+++ CA231018/CA231018/Program.cs
@@ -0,0 +1,63 @@
+﻿using CA231018;
+using System.Text;
+
+static int ByteMeret(List<Request> requests) 
+{
+    return requests
+        .Sum(r => r.Meret);
+}
+static bool Domain(Request ember) 
+{
+    if (char.IsDigit(ember.Cim[ember.Cim.Length - 1]))
+        return false;
+    else
+        return true;
+}
+static int DomainesDB(List<Request> requests) 
+{
+    var domainesek = new List<Request>();
+
+    for (int i = 0; i < requests.Count; i++)
+    {
+        if (Domain(requests[i]))
+            domainesek.Add(requests[i]);
+    }
+
+    return domainesek.Count;
+}
+static Dictionary<string, int> Statisztika(List<Request> requests) 
+{
+    var f9 = requests.GroupBy(gb => gb.HttpKod);
+
+    var f9dic = new Dictionary<string, int>();
+
+    foreach (var item in f9)
+    {
+        f9dic.Add(item.Key, item.Count());
+    }
+
+    return f9dic;
+}
+
+
+var sr = new StreamReader(
+    path: @"..\..\..\src\NASAlog.txt",
+    encoding: Encoding.UTF8);
+
+var requests = new List<Request>();
+
+while (!sr.EndOfStream)
+    requests.Add(new Request(sr.ReadLine()));
+
+var f5 = requests.Count();
+
+Console.WriteLine($"5. feladat: Kérések száma: {f5}");
+Console.WriteLine($"6. feladat: Válaszok összes mérete: {ByteMeret(requests)} byte");
+Console.WriteLine($"8. feladat: Domain-es kérések: {((double)DomainesDB(requests) / (double)f5) * 100:.00}%");
+Console.WriteLine($"9. feladat: Statisztika:");
+
+foreach (var item in Statisztika(requests))
+    Console.WriteLine($"\t {item.Key}: {item.Value} db");
+
+
+Console.ReadKey();
---
+++ CA231018/CA231018/request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231018
+{
+    class Request
+    {
+        public string Cim { get; set; }
+        public string DatumIdo { get; set; }
+        public string Get { get; set; }
+        public string HttpKod { get; set; }
+        public int Meret { get; set; }
+
+        public Request(string sor)
+        {
+            var atmeneti = sor.Split('*');
+            this.Cim = atmeneti[0];
+            this.DatumIdo = atmeneti[1];
+            this.Get = atmeneti[2];
+            var atmeneti2 = atmeneti[3].Split(' ');
+            this.HttpKod = atmeneti2[0];
+            if (atmeneti2[1] == "-")
+                this.Meret = 0;
+            else
+                this.Meret = int.Parse(atmeneti2[1]);
+        }
+    }
+}
